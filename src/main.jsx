2ef12d6fdf864a6c32dce48f6324230d06bf980d
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-05-20 18:44:00 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ fucntion App() {
   return (
     <div>
       <h1>Page title</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores, nulla. At impedit repellendus libero reprehenderit dicta eveniet, quod reiciendis maxime officia possimus ipsum doloremque tempore, sint necessitatibus inventore perferendis porro!</p>
+      <button>Click me</button>
     </div>
   );
 }
