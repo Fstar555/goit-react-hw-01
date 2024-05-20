741d39b2cbe07853ba556890eb25f8f35e316efa
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-05-20 18:35:23 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+fucntion App() {
+  return (
+    <div>
+      <h1>Page title</h1>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App/>
+  </React.StrictMode>
+);
